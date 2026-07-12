e30a2f601cad9e348deaa848b9ae5a350ed3de39
heesoo <shs02008@gmail.com>
2026-07-12 17:07:20 +0900

    메뉴 줄 추가

--- Code.js
+++ Code.js
@@ -177,6 +177,7 @@ function onOpen() {
     //.addItem('시트 순서 / 숨김 정리', 'organizeSheetTabs_')
     //.addItem('현황판 새로고침', 'refreshDashboard')
     .addItem('제출물 원문 열 보이기/숨기기', 'toggleExtractedTextColumn')
+    .addSeparator()
     .addToUi();
 }
 
